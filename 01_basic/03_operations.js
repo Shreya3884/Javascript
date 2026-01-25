abb7c5f0f88a6920ff8ad510fd57842ec350db1d
Shreya3884 <sj0711778@gmail.com>
2026-01-26 00:09:30 +0530

    commit

---
+++ 01_basic/03_operations.js
@@ -0,0 +1,24 @@
+// let value=3
+// let nrgative_value=-value;
+// console.log(nrgative_value); //-3
+
+// console.log("1"+2) //12
+// console.log(1+"2") //12
+// console.log("1"+2+2) //122
+// console.log(1+2+"2"+"2")  
+// console.log("1"+2+2+"2")  //1222
+
+// // console.log(true+);  //error
+// // console.log(+true);    //1
+// console.log(+"")  //0
+// preincrement -> phle value ko increment krega fr usko return krega
+// let a=5;
+// let b=++a;
+// console.log(a);  //6
+// console.log(b);  //6
+// PostIncrement -> phle value assign hoi fr increment hogi
+let a=5;
+let b=a++;
+console.log(a);  // phle b m value assign hogi jo ki 5 h and then a m increment hoga
+console.log(b);
+// a=6 and b=5
