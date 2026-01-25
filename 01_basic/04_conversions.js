c547735b31dc3835aa44f51d867102c08f44b399
Shreya3884 <sj0711778@gmail.com>
2026-01-26 00:17:35 +0530

    commit

---
+++ 01_basic/04_conversions.js
@@ -0,0 +1,15 @@
+console.log("2">1); //true
+console.log(null<0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+
+console.log(undefined==0);   //false
+console.log(undefined>0);    //false
+console.log(undefined>=0);  //false
+
+
+//===
+// value and their datatypes also checked
+console.log("2"===2);  //false
+console.log(null===undefined); //false
+console.log(2=="2"); //true
