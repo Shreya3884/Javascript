adb1cdaf79e38fcd483e02e18da577f2f30337dc
Shreya3884 <sj0711778@gmail.com>
2026-01-25 10:40:55 +0530

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+// ........................Primitive datatypes............
+// number=2 to the power of 53
+// big int
+// string=""
+//boolean=true/false
+//null=standalone value
+//undefined=>value is not assign
+//symbol=unique
+
+// ........................Non-Primitive datatypes............
+// objects
+
+// to know the type of we use typeof operator
+
+console.log(typeof null)     // object
+console.log(typeof undefined)   // undefined
+console.log(typeof object)      //undefined
+console.log(typeof symbol)     //undefined
+console.log(typeof number)     //undefined
+console.log(typeof bigint)     //undefined
