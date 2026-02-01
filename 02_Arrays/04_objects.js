90a17fdafdeb4269d1aeea896938c2a6532efa7f
Shreya3884 <sj0711778@gmail.com>
2026-02-01 06:07:10 +0530

    destructuring

---
+++ 02_Arrays/04_objects.js
@@ -0,0 +1,25 @@
+const course={
+    coursename:"javascript",
+    price:999,
+    cousreteacher:"shreya",
+}
+//course.coursename
+
+const {coursename}=course   //DESTRUCTURING
+console.log(coursename)  //javascript
+
+// const {coursename:name}=course
+// console.log(name) //javascipt
+
+// {
+//     "name":"shreya",
+//     "age":"66",
+//     "coursename":"javascript",  //json format
+// }
+
+[
+    {},
+    {},
+]
+
+
