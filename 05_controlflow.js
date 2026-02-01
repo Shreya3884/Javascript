3c2c505e643a26c3ab6a4cd61f8bb071f98a9e24
Shreya3884 <sj0711778@gmail.com>
2026-02-01 18:05:26 +0530

    control flow

---
+++ 05_controlflow.js
@@ -0,0 +1,55 @@
+// if(condition){
+//     //statements
+// }else if(condition){
+//     //statements
+// }else{
+
+// }
+
+// switch(condition to be checked){
+//     case value:
+//         break;
+//         case value:
+//             break;
+//             default:
+//                 break;
+// }
+
+
+//////Truthy values..//////
+
+// const useremail="shreya@gmail.com";
+// if(useremail){
+//     console.log("got user email");
+// }else{
+//     console.log("dont have user email");
+// }
+
+// // empty string -> false
+// //empty array ->true
+// //FALSY VALUE->
+
+// false,0,-0,BigInt 0n,"",null,undefined,nan
+
+// //Truthy values
+// (),[],"shreya","0","false",{},function(){},
+
+
+// //false==0 true
+// //false=="" true
+// //0=="" true
+//NULL COLAECING OPERATOR
+let val1;
+// val1=5??10  //no output
+val1=null??10 //10
+val2=undefined??10 //10
+console.log(val2);
+
+
+//TERNIARY OPERATOR
+
+// condition?true:false
+
+const icetea=5
+icetea==5?console.log("right"):console.log("false")
+
