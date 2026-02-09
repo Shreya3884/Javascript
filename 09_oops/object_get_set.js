588b5f976aed395dbb241d720b9544a9da5d46e9
Shreya3884 <sj0711778@gmail.com>
2026-02-09 23:50:25 +0530

    getter setter

---
+++ 09_oops/object_get_set.js
@@ -0,0 +1,15 @@
+const user={
+    _email:"h@hc.com",
+    _password:"abc",
+
+    get email(){
+       return this._email.toUpperCase()
+    },
+    set email(value){ //email is just a property email and _email is same
+       this._email=value   
+    }
+}
+
+const tea=Object.create(user)
+console.log(tea._password)
+console.log(tea.email)
